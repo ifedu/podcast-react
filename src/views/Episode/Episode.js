0d8ca2a3e008607e905bb6d8b617ec2497fd9684
Edu Pérez-Bermejo <ifedu@outlook.com>
2023-03-16 14:22:43 +0100

    feat(Episode): add functionality to Episode view

--- src/views/Episode/Episode.js
+++ src/views/Episode/Episode.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
+
+import { PodcastInfo } from '../../components/PodcastInfo/PodcastInfo';
 
 import './Episode.css';
 
 export function Episode() {
+  const { state: { episode, podcastInfo, song } } = useLocation();
+
   return (
     <div className='Episode'>
-      Episodio
+      <PodcastInfo info={podcastInfo.results[0]} song={song}/>
+
+      <div className='info'>
+        <div className='title'>{episode.trackName}</div>
+        <div className='description' dangerouslySetInnerHTML={{ __html: episode.description }}></div>
+        <div className='audio'>
+          <audio controls>
+            <source src={episode.episodeUrl}/>
+          </audio>
+        </div>
+      </div>
     </div>
   );
 }
