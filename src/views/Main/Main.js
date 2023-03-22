4ac3394a15bc3f32b28b51673f310b8e21fd8102
Edu Pérez-Bermejo <ifedu@outlook.com>
2023-03-22 11:41:37 +0100

    fix(Main): update the number of songs when are filtered

--- src/views/Main/Main.js
+++ src/views/Main/Main.js
@@ -8,52 +8,50 @@ import { useSongs } from '../../hooks/useSongs';
 export function Main() {
   const { setIsLoading } = useLoading();
   const [filterValue, setFilter] = React.useState('');
-
   const songs = useSongs(setIsLoading);
+
+  function getSongsFiltered() {
+    return songs?.entry.filter((song) =>
+      song['im:name'].label.toUpperCase().includes(filterValue.toUpperCase()) ||
+        song['im:artist'].label.toUpperCase().includes(filterValue.toUpperCase())
+    )
+  }
 
   function onFilterChange(e) {
     setFilter(e.target.value);
   }
 
-  if (!songs) {
-    return null;
-  }
+  if (!songs) return null;
+
+  const filterSongs = getSongsFiltered();
 
   return (
     <div className='Main' data-testid='main'>
       <div className='filter'>
-        <span>{songs?.entry?.length}</span>
+        <span>{filterSongs?.length}</span>
         <input placeholder='Filter podcasts...' onChange={onFilterChange}/>
       </div>
 
       <div className='cols'>
         {
-          songs?.entry.map((song, i) => {
-            if (
-              filterValue === '' ||
-              song['im:name'].label.toUpperCase().includes(filterValue.toUpperCase()) ||
-              song['im:artist'].label.toUpperCase().includes(filterValue.toUpperCase())
-            ) {
-              return <Link key={i} to={`/podcast/${song.id.attributes['im:id']}`} state={{ song }}  data-testid='link-to-podcast'>
-                <div className='col'>
-                  <div className='song'>
-                    <div className='background'></div>
+          filterSongs?.map((song, i) =>
+            <Link key={i} to={`/podcast/${song.id.attributes['im:id']}`} state={{ song }}  data-testid='link-to-podcast'>
+              <div className='col'>
+                <div className='song'>
+                  <div className='background'></div>
 
-                    <div className='image'>
-                      <img src={song['im:image'][2].label} alt='All Songs Considered'/>
-                    </div>
+                  <div className='image'>
+                    <img src={song['im:image'][2].label} alt='All Songs Considered'/>
+                  </div>
 
-                    <div className='texts'>
-                      <div className='title'>{song['im:name'].label}</div>
-                      <div className='author'>Author: {song['im:artist'].label}</div>
-                    </div>
+                  <div className='texts'>
+                    <div className='title'>{song['im:name'].label}</div>
+                    <div className='author'>Author: {song['im:artist'].label}</div>
                   </div>
                 </div>
-              </Link>
-            }
-
-            return null;
-          })
+              </div>
+            </Link>
+          )
         }
       </div>
     </div>
